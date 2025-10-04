254b0e9681ffdd1877516aedb0c0b9a5ce8437a2
ven0m <yermolenko79@gmail.com>
2025-10-04 17:16:04 +0300

    add boxnpe

--- app/components/molecules/BoxAdditional/page.tsx
+++ app/components/molecules/BoxAdditional/page.tsx
@@ -7,14 +7,26 @@ import Image from "next/image";
 const BoxAdditional = () => {
 
   return (
-<div className="flex flex-col w-[280px] h-[200px] items-center border-l border-gray-200">
-      <h2 className="text-black uppercase">Благодійний фонд</h2>
-            <Image
-              src="/fond-nanu.webp"
-              width={200}
-              height={87}
-              alt="Logo NANU 100"
-            />
+    <div className="flex flex-col w-[280px] items-center border-l border-gray-200">
+      <div className="flex flex-col py-2 items-center">
+        <h2 className="text-black uppercase">Благодійний фонд</h2>
+        <Image
+          src="/fond-nanu.webp"
+          width={200}
+          height={87}
+          alt="Logo NANU 100"
+        />
+      </div>
+      <div className="flex flex-col py-2 items-center">
+        <h2 className="text-black">Ядерна енергетика та довкілля</h2>
+        <Image
+          src="/titul-npe.webp"
+          width={150}
+          height={198}
+          alt="Logo NPE"
+        />
+      </div>
+
     </div>
 
   );
