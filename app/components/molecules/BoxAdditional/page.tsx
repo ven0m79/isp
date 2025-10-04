d183296ef2df2b8f908d15c9a3d8824c959afc40
ven0m <yermolenko79@gmail.com>
2025-10-04 11:31:40 +0300

    add boxAdditional

--- app/components/molecules/BoxAdditional/page.tsx
+++ app/components/molecules/BoxAdditional/page.tsx
@@ -7,8 +7,14 @@ import Image from "next/image";
 const BoxAdditional = () => {
 
   return (
-    <div className="flex w-[280px] h-auto justify-center border-l border-gray-200">
+<div className="flex flex-col w-[280px] h-[200px] items-center border-l border-gray-200">
       <h2 className="text-black uppercase">Благодійний фонд</h2>
+            <Image
+              src="/fond-nanu.webp"
+              width={200}
+              height={87}
+              alt="Logo NANU 100"
+            />
     </div>
 
   );
