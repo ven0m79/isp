330e6eaf32fca8c6c390b6194d5d2bc2acdbb207
ven0m <yermolenko79@gmail.com>
2026-04-21 19:22:00 +0300

    fix: layout1

--- app/components/templates/main-layout/MainLayout.tsx
+++ app/components/templates/main-layout/MainLayout.tsx
@@ -1,24 +1,61 @@
-import { Roboto } from "next/font/google";
-import "./globals.css";
+import React, { FC, ReactNode } from "react";
 
-const roboto = Roboto({
-  weight: ["400", "700"],
-  subsets: ["latin", "cyrillic"],
-  display: "swap",
-});
+import Nav from "../../molecules/Nav/page";
+import Footer from "../../molecules/Footer/page";
+import Header from "../../molecules/Header/page";
+import Slider from "../../molecules/Slider/page";
+import BoxNanu from "../../molecules/BoxNanu/page";
+import BoxAdditional from "../../molecules/BoxAdditional/page";
 
-export default function RootLayout({
+type MainLayoutProps = {
+  children: ReactNode;
+  noHeader?: boolean;
+  noSlider?: boolean;
+  noNav?: boolean;
+  noFooter?: boolean;
+  noBoxNanu?: boolean;
+  noBoxAdditional?: boolean;
+};
+
+const MainLayout: FC<MainLayoutProps> = ({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  noHeader = false,
+  noNav = false,
+  noFooter = false,
+  noSlider = false,
+  noBoxNanu = false,
+  noBoxAdditional = false,
+}) => {
   return (
-    <html lang="en">
-      <body className={`flex min-h-screen overflow-x-hidden bg-gray-950 text-gray-50 ${roboto.className}`}>
-        <main className="flex flex-1 flex-col w-full">
-          {children}
-        </main>
-      </body>
-    </html>
+    <main>
+      {/* Загальна “коробка” з тінню для всього контенту, включно з футером */}
+      <div className="flex flex-col w-full max-w-300 mx-auto flex-1 bg-white my-5 layer-shadow">
+        {!noHeader && <Header />}
+        <section className="flex w-full">
+          {!noSlider && <Slider />}
+          {!noBoxNanu && <BoxNanu />}
+        </section>
+        {/* Навігація */}
+        {!noNav && (
+          <div className="relative -mt-12 z-50 self-center bg-blue-300 rounded-[20px]">
+            <Nav />
+          </div>
+        )}
+        <div className="flex-1 flex w-full h-auto mt-8">
+          <div className="flex-1 p-2 z-40">
+            {children}
+          </div>
+          <aside className="relative flex z-40">
+            {!noBoxAdditional && <BoxAdditional />}
+          </aside>
+        </div>
+        {!noFooter && <Footer />}
+      </div>
+      <div className="flex justify-center mb-6 text-black text-[14px]">
+      {"©"} {new Date().getFullYear()} {"Інститут проблем безпеки атомних електростанцій. Всі права захищені"}
+      </div>
+    </main>
   );
 }
+
+export default MainLayout;
